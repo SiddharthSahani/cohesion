f83ff57f5ef14efb8de23f10ba1d16a2f05f3ec1
TejasBhovad <tejasbhovad@gmail.com>
2024-11-27 17:41:35 +0530

    feat: submit button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import SubmitButton from '../components/SubmitButton.svelte';
+</script>
+
+<div class="h-full w-full bg-gray-900">
+	<SubmitButton />
+</div>
