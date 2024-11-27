38e28dd258785d4fb2ded6c5f138d01d92e58c6f
SiddharthSahani <sidhucroedde@gmail.com>
2024-11-27 19:17:54 +0530

    refactor: removed unused style from submit button

--- src/components/SubmitButton.svelte
+++ src/components/SubmitButton.svelte
@@ -10,42 +10,20 @@
 	class="relative flex h-auto gap-1 overflow-hidden rounded border border-white/25 bg-black/75 p-1 pr-4 font-bold text-white"
 >
 	<div
-		class="chevdiv absolute left-0 top-0 flex h-full transform items-center justify-center rounded-sm bg-yellow-400 text-black transition-all duration-300 ease-in-out"
+		class="chevdiv absolute left-0 top-0 flex h-full transform items-center justify-center rounded-sm bg-yellow-400 text-black"
 	>
-		<ChevronsRight class="chevron" strokeWidth="3" />
+		<ChevronsRight strokeWidth="3" />
 	</div>
 	<span class="text relative z-10 pl-8">Submit</span>
 </button>
 
 <style>
-	button {
-		position: relative;
-		overflow: hidden;
-	}
 	.chevdiv {
 		width: 2rem; /* Adjust this width to fit the chevron */
 		transition: width 0.3s ease-in-out;
-		display: flex;
-		align-items: center;
 	}
 	button:hover .chevdiv {
 		width: 100%; /* Adjust this to cover the remaining space */
-	}
-	.chevron {
-		opacity: 1;
-		transition: opacity 0.3s ease-in-out;
-	}
-	.hover-only {
-		opacity: 0;
-		animation: fadeIn 0.3s forwards;
-	}
-	button:hover .hover-only {
-		animation: fadeIn 0.3s forwards;
-	}
-	@keyframes fadeIn {
-		to {
-			opacity: 1;
-		}
 	}
 	.text {
 		transition:
