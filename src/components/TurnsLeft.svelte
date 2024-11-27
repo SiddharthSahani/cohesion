a0c60dd52f413c2191810c8408b2eddb4f3abcd9
SiddharthSahani <sidhucroedde@gmail.com>
2024-11-27 20:01:23 +0530

    feat: turns left component

---
+++ src/components/TurnsLeft.svelte
@@ -0,0 +1,38 @@
+<script>
+    let { turnsLeft, totalTurns = 5 } = $props();
+</script>
+
+{#snippet TurnDiv({ left })}
+    {#if left}
+        <div class="oscillating-border h-12 w-12 rounded-full bg-amber-400"></div>
+    {:else}
+        <div class="oscillating-border h-12 w-12 rounded-full bg-stone-800"></div>
+    {/if}
+{/snippet}
+
+<div class="flex w-min gap-5 rounded-md bg-gray-800/20 px-3 py-1">
+    {#each Array(totalTurns) as _, i}
+        {@render TurnDiv({ left: i < turnsLeft })}
+    {/each}
+</div>
+
+<style>
+    @keyframes oscillate {
+        0% {
+            border-radius: 50%;
+            margin: 0px -8px;
+        }
+        50% {
+            border-radius: 0%;
+            margin: 0px -8px;
+        }
+        100% {
+            border-radius: 50%;
+            margin: 0px -8px;
+        }
+    }
+
+    .oscillating-border {
+        animation: oscillate 2s infinite;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import SubmitButton from '../components/SubmitButton.svelte';
+    import TurnsLeft from '../components/TurnsLeft.svelte';
 </script>
 
 <div class="h-full w-full bg-gray-900">
     <SubmitButton />
+    <TurnsLeft turnsLeft={4} totalTurn={6} />
 </div>
