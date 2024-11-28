de5ad8edf52575808a049692f53fb5c18046a839
SiddharthSahani <sidhucroedde@gmail.com>
2024-11-28 19:49:14 +0530

    chore: light-dark theme button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,17 @@
 <script>
+    import { Sun } from 'lucide-svelte';
     import '../app.css';
     import CohesionLogo from '../components/CohesionLogo.svelte';
     let { children } = $props();
 </script>
 
 <div class="flex h-full w-full flex-col justify-between bg-background p-4">
-    <CohesionLogo />
+    <div class="flex justify-between">
+        <CohesionLogo />
+        <button onclick={() => alert('flashbang ')} class="rounded-lg bg-tertiary p-1 text-primary">
+            <Sun size={32} strokeWidth={2} />
+        </button>
+    </div>
     <div class="flex flex-col items-center justify-center">
         {@render children()}
     </div>
