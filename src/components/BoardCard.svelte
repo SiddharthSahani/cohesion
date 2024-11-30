922fd74d0cebe3412fa22758fa278b4d9bb3a981
SiddharthSahani <sidhucroedde@gmail.com>
2024-11-30 20:36:12 +0530

    chore: navigate to another page when a trending board is clicked

--- src/components/BoardCard.svelte
+++ src/components/BoardCard.svelte
@@ -4,10 +4,14 @@
     let { id, name, timesPlayed } = $props();
 </script>
 
-<div class="flex w-full justify-between rounded-xl bg-secondary p-6">
+<a
+    class="flex w-full justify-between rounded-xl bg-secondary p-6"
+    href="/{id}"
+    data-sveltekit-preload-data="tap"
+>
     <div class="flex gap-3">
         <CohesionIcon />
         <div class="text-xl font-semibold text-white">{name}</div>
     </div>
     <div class="text-xl font-semibold text-white/50">{timesPlayed} matches played</div>
-</div>
+</a>
